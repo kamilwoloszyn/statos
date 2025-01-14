10519678011c1cac18c0e9fbb9d1a29d365c95cc
Kamil Woloszyn <kamil.woloszyn1@gmail.com>
2025-01-14 21:36:57 +0100

    cleanup

--- app/commander.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import "io"
-
-// Commander is a generic interface that executes commands
-type Commander interface {
-	Execute(cmd string, args ...string) ([]byte, error)
-	ExecuteWithPipe(cmd string, args ...string) (io.ReadCloser, error)
-}
